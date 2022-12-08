b1e4c473f4034f0d2c96e913cc09ace40eab2b59
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-08 15:46:23 +0000

    resolve task 4 and 5 to betty standard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-	/**
-	 * main - Entry point
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int main(void)
-	{
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-		puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 
-		return (0);
-	}
+	return (0);
+}
