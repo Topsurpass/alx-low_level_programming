3f0cfb3bb7029488de5873a317df3b51aee97eb0
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-08 14:53:32 +0000

    third commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 	/**
 	 * main - Entry point
 	 *
 	 * Return: Always 0 (Success)
 	 */
-
 	int main(void)
 	{
-		char strng[] = "\"Programming is like building a multilingual puzzle\0";
-		puts(strng);
+
+		puts("\"Programming is like building a multilingual puzzle");
 
 		return (0);
 	}
