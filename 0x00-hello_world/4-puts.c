040ca2692bbb42f5f6095c5ddf49bdd44c6166c6
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-08 14:35:19 +0000

    second commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
+	/**
+	 * main - Entry point
+	 *
+	 * Return: Always 0 (Success)
+	 */
+
 	int main(void)
 	{
-		char strng[] = "Programming is like building a multilingual puzzle\0";
-
+		char strng[] = "\"Programming is like building a multilingual puzzle\0";
 		puts(strng);
 
 		return (0);
