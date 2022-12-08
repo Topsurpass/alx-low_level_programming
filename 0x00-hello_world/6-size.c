02112b698416d9eacc252382e02f6c82d5942761
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-08 16:06:52 +0000

    task 6 resolved

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
 	printf("Size of a int: %lu bytes(s)\n", sizeof(int));
-	printf("Size of a long: %lu bytes(s)\n", sizeof(long));
-	printf("Size of a long long: %lu bytes(s)\n", sizeof(long long));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
 	return (0);
 }
