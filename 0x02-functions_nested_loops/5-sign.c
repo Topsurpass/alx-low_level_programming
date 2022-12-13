7dcaafe3ebad725093bb837d1294aa779f2d0f25
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-13 15:04:58 +0000

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar('\n');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar('\n');
 		return (-1);
 	}
 	_putchar('\n');
