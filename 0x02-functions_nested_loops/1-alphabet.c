34759d868ef9354c2cf74374aeef644962c04ea0
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-13 11:54:17 +0000

    second commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * main - The entry poin for our program
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+	return (0);
+}
