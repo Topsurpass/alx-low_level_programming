019409ad981a70ce0eff53c11ac457a68c4a13ca
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-14 15:33:31 +0000

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main - Test function that prints if integer is positive 
- * or negative
+ * main - Test number if zero positive or negative
  * Return: 0
  */
 
@@ -9,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
