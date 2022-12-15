b302ba98841b40fc97e160e8e175a40b7e292853
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-15 17:31:58 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,20 +5,25 @@
  * multiple of 5 as Buzz and multiple of both
  * 3 and 5 as FizzBuzz
  */
-void FizzBuzz(void)
+int main(void)
 {
-	int i;
-
-	for (i = 1; i <= 100; i++)
+	void FizzBuzz(void)
 	{
-		if ((i % 3) == 0)
-			printf("Fizz ");
-		else if ((i % 5) == 0)
-			printf("Buzz ");
-		else if ((i % 15) == 0)
-			printf("FizzBuzz ");
-		else
-			printf("%d ", i);
+		int i;
+
+		for (i = 1; i <= 100; i++)
+		{
+			if ((i % 3) == 0)
+				printf("Fizz ");
+			else if ((i % 5) == 0)
+				printf("Buzz ");
+			else if ((i % 15) == 0)
+				printf("FizzBuzz ");
+			else
+				printf("%d ", i);
+		}
+		printf("\n");
 	}
-	printf("\n");
+	FizzBuzz();
+	return (0);
 }
