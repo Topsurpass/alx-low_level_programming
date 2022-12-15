17531e09dd698389580cfd6d154652894e4a6d3f
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-15 13:18:34 +0000

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,9 +2,11 @@
  * mul - Multiply two numbers
  * @a: First argument of type integer
  * @b: Second argument of type integer
+ * Return: product of the 2 arguments
  */
 int mul(int a, int b)
 {
 	int product = (a * b);
+
 	return (product);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
-int isdigit(int args);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
