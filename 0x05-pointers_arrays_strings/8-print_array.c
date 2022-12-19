2c83102e9d7bb4c20ba201a228117970051e4a08
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-19 22:15:27 +0000

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		if (i != (n - 1))
+		if (i != n - 1)
 			printf("%d, ", a[i]);
 		else
 			printf("%d", a[i]);
-		i++;
 	}
 	printf("\n");
 }
