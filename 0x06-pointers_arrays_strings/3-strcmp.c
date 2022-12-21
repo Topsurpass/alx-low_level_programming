227213bc610f3ea05b8e0b26eb3a64f03a7bb08d
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-21 23:11:54 +0000

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,10 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int j = 0;
 
-	for (i = 0; *(s1 + j) == *(s2 + j); i++)
-	{
+	for (i = 0; *(s1 + i) == *(s2 + i) && *(s1 + i); i++)
 		j++;
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
+	else
 		return (0);
-	}
-	if (*(s2 + j))
-		return (*(s1 + j) - *(s2 + j));
 }
