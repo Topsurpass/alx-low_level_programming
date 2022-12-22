65fa30dc4d457ef56507ddc48a9ac11b30f1bac6
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2022-12-22 21:43:41 +0000

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -24,7 +24,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (l + 1 > size_r)
 		return (0);
 	r[l] = '\0';
-	for (k = (l - 1); k >= 0; k--)
+	for (k = l - 1; k >= 0; k--)
 	{
 		i--;
 		j--;
