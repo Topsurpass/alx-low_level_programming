98e5beb3e15e416d1a208f2ebe2a668708d72de9
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-02 19:32:43 +0000

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,17 +9,18 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i = 0, j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	while (needle[i] != '\0')
 	{
 		for (j = 0; haystack[j] != '\0'; j++)
 		{
 			if (needle[i] == haystack[j])
+			{
 				return ((haystack + j));
-			if (haystack[j + 1] == '\0' && needle[i] != haystack[j + 1])
-				return (NULL);
+			}
 		}
+		i++;
 	}
 	return (NULL);
 }
