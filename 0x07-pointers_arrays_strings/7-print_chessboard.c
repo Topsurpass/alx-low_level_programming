21247b373adb1272b4c45570ad9866c76d9ab3f9
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-02 15:01:58 +0000

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,21 +9,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j, k;
+	int i = 0, j;
 
 	while (i < 8)
 	{
-		if (i == 0 || i == 1 || i == 6 || i == 7)
+		for (j = 0; j < 8; j++)
 		{
-			for (j = 0; j < 8; j++)
-			{
-				_putchar(a[i][j]);
-			}
-		}
-		else if (i >= 2 && i <= 5)
-		{
-			for (k = 0; k < 8; k++)
-				_putchar(' ');
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 		i++;
