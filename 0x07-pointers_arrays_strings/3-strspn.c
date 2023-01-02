aacd8ac263250d7765ca7f0aeb9f9bff5517ebde
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-02 13:18:08 +0000

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,17 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	unsigned int len = 0;
 
-	for (i = 0; s[i] != ','; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				len++;
+				break;
+			}
+			if(accept[j + 1] == '\0' && s[i] != accept[j])
+				return (len);
 		}
 	}
 	return (len);
