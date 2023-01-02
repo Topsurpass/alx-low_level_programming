e38af4ea9e9c9f1d15b235bbcbbb24ff1038ebd0
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-02 12:57:16 +0000

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,12 @@
 char *_strchr(char *s, char c)
 {
 
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0' && s[i]; i++)
-	{
-		if (s[i] == c)
-			return ((s + i));
-	}
-	return (NULL);
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	if (s[i] == c)
+		return ((s + i));
+	else
+		return (NULL);
 }
