662088f1f1a88c6de6f32121be4b1d7cbab3cdb6
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-06 14:27:02 +0000

    resolved negative numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] >= 48 || *argv[i] <= 57)
+			if (*argv[i] >= '0' || *argv[i] <= '9')
 				sum += atoi(argv[i]);
 			if (*argv[i] < 48 || *argv[i] > 57)
 			{
