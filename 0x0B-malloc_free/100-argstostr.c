7f47206a70442d1a919fbe7dc41b9b6695bdbfd3
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-11 14:29:29 +0000

    tried with another method

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,47 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
- * _strlen - length of string
+ * _strlen - get the length of string
  * @s: the string
  *
- * Return:  the length of string
+ * Return: the length of string
  */
-
 int _strlen(char *s)
 {
-	int i = 0;
+	int a = 0;
 
-	if (s[i] == '\0')
+	if (s[a] == 0)
+	{
 		return (0);
-	i++;
-	return (i + _strlen(s + i));
+	}
+	a++;
+	return (a + _strlen(s + a));
 }
 /**
- * argstostr - Concatenate all the arguments of
- * my program
- * @ac: argument counter
- * @av: argument array
+ * argstostr - concatenates program arguments
+ * @ac: argument count
+ * @av: argument arrays
  *
- * Return: pointer to char (new modified string)
+ * Return: concatenated string
  */
 char *argstostr(int ac, char **av)
 {
 	char *arr;
-	int i, j, length, k, l;
-
-	length = 0;
-	l = 0;
+	int i, j;
+	int l = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		length += _strlen(av[i]);
-	}
-	length += (ac + 1);
-	arr = malloc(sizeof(char) * length);
+	arr = malloc(sizeof(char *) * (ac + ac));
 
 	if (arr == NULL)
 	{
@@ -49,13 +41,15 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	}
 
-	for (j = 0; j < ac; j++)
+	for (i = 0; i < ac; i++)
 	{
-		for (k = 0; k < _strlen(av[j]); k++)
+
+		for (j = 0; j < _strlen(av[i]); j++)
 		{
-			arr[l++] = av[j][k];
+			arr[l++] = av[i][j];
 		}
 		arr[l++] = '\n';
 	}
 	return (arr);
+
 }
