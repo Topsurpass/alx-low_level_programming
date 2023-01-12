eae8a79722a57c6e1f8c031c793d0a61264d7302
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-12 15:58:33 +0000

    used int for sizeof inplace of pointer dereference

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,12 +17,12 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (max - min) + 1);
+	arr = malloc(sizeof(int) * (max - min + 1));
 
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; max > min; i++)
+	for (i = 0; max >= min; i++)
 	{
 		arr[i] = min;
 		min += 1;
