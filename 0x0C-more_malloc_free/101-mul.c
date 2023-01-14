e4bf5a7f620fa48cd3f9921d4cd5781b144b44d0
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-14 01:46:30 +0000

    initial commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - get length of string
+ * @s: the string
+ *
+ * Return: the string length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return (0);
+	i++;
+	return (i + _strlen(s + i));
+}
+/**
+ * print_char - print characters
+ * @s: the string
+ *
+ */
+void print_char(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+/**
+ * _alloc - allocate space
+ * @size: size of memory block in bytes
+ *
+ * Return: pointer to char
+ */
+void *_allocator(unsigned int size)
+{
+	void *arr;
+
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size);
+
+	if (arr == NULL)
+		return (NULL);
+	else
+		return (arr);
+}
+/**
+ * rmv_lead_0 - remove leading zero
+ * @arr: the string
+ * @length_1: the length of string 1
+ * @length_2: the length of string 2
+ *
+ * Return: nothing
+ */
+void rmv_lead_0(char *arr, int length_1, int length_2)
+{
+	int i, j;
+
+	i = 0;
+
+	while (arr[i] == '0')
+		i++;
+	if (i == length_1 + length_2)
+	{
+		arr[1] = '\0';
+		arr[0] = '0';
+	}
+	else
+	{
+		j = 0;
+		for (; arr[i] != '\0'; j++, i++)
+			arr[j] = arr[i];
+		arr[j] = '\0';
+	}
+}
+/**
+ * multiply - multiply digits stored in array
+ * of characters
+ * @num_1: the string 1 array
+ * @num_2: the string 2 attay
+ * @answer: the result of multiplication of
+ * string 1 and 2
+ * Renurn: nothing
+ */
+
+void multiply(char num_1[], char num_2[], char answer[])
+{
+	int len_1 = _strlen(num_1);
+	int len_2 = _strlen(num_2);
+	int keep = 0;
+	int i, j, temp, product;
+	char *err = "Error";
+
+	for (i = 0; i < (len_1 + len_2); i++)
+		answer[i] = '0';
+	answer[len_1 + len_2] = '\0';
+	for (i = (len_1 - 1); i >= 0; i--)
+	{
+		if ((num_1[i] < '0') || (num_1[i] > '9'))
+		{
+			print_char(err);
+			exit(98);
+		}
+		else
+		{
+			keep = 0;
+			for (j = (len_2 - 1); j >= 0; j--)
+			{
+				if ((num_2[j] < '0') || (num_2[j] > '9'))
+				{
+					print_char(err);
+					exit(98);
+				}
+				product = (num_1[i] - '0') * (num_2[j] - '0');
+				temp = (answer[i + j + 1] - '0') + product + keep;
+				answer[i + j + 1] = temp % 10 + '0';
+				keep = temp / 10;
+			}
+		}
+		answer[i] += keep;
+	}
+	rmv_lead_0(answer, len_1, len_2);
+}
+
+/**
+ * main - multiplies 2 positive numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 (success), 98 (incorrect argument)
+ */
+
+int main(int argc, char *argv[])
+{
+	char *answer;
+	char *error = "Error";
+	int len_1, len_2;
+
+	len_1 = _strlen(argv[1]);
+	len_2 = _strlen(argv[2]);
+
+	if (argc < 3 || argc > 3)
+	{
+		print_char(error);
+		exit(98);
+	}
+
+	answer = _allocator(sizeof(*answer) * (len_1 + len_2 + 1));
+
+	multiply(argv[1], argv[2], answer);
+	print_char(answer);
+
+	free(answer);
+	return (0);
+}
