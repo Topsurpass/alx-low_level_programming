f581d4151b3609110b9278c72a63a409bd33e089
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-16 14:17:28 +0000

    validate if struct is initialized correctly

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - initialize variable of type struct dog
  * @d: the struct to initialize
@@ -10,6 +11,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
