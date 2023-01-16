720c43750b61c52c73f49e6d4f63b9157c2a9adb
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-16 13:01:07 +0000

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - The details of dogs
+ * @name: the dog name
+ * @age: the dog's age
+ * @owner: the dog owner
+ *
+ * Description: This tells the owner of the dog with
+ * the following dog details such as its name and age
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
