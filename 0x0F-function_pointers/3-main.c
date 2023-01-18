68be72d6f8eb958bffe98d3bd543821546f06fde
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-18 22:37:57 +0000

    changed the if condition

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,17 +20,19 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	func = get_op_func(argv[2]);
 
-	if (((func == NULL) || (argv[2][1] != '\0')))
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && (argv[3][0] == '0'))
+
+	func = get_op_func(argv[2]);
+
+	if (func == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
 	num1 = atoi(argv[1]);
