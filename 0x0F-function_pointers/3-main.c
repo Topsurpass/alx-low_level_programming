dfe0f30c1531db9441f6659ab3339305074f8518
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-18 22:47:10 +0000

    resolved

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,23 +20,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	func = get_op_func(argv[2]);
-
-	if (func == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+
+	if (func == NULL || (argv[2][1] != '\0'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 
 	printf("%d\n", func(num1, num2));
 
