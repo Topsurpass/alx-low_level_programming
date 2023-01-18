baf3abb5935e54298d7285c4b807c2412062d1cf
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-18 22:15:03 +0000

    resolved error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,14 +24,12 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	func = get_op_func(argv[2]);
 
-	if (((func == NULL) || (argv[2][1] != '\0')) && ((argv[2][0] != '*')
-				&& (argv[2][0] != '+') && (argv[2][0] != '-') &&
-				(argv[2][0] != '/') && (argv[2][0] != '%')))
+	if (((func == NULL) || (argv[2][1] != '\0')))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (argv[3][0] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
