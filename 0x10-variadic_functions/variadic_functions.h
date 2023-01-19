0d32f0bc89138c4e51dca013ee4aebfbe2de34bc
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-01-19 15:00:23 +0000

    adjusted header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,13 @@
 #ifndef _VARIADIC_FUNCTIONS
 #define _VARIADIC_FUNCTIONS
 
-#include <stdlib.h>
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
