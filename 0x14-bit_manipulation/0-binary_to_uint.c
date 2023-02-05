e7bbe6cb24aa9a684c5d9976c6bde6fcc8adbc45
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-02-05 06:25:10 +0000

    defined my strlength function with my left shift operator

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,19 @@
 #include "main.h"
+/**
+ * _strlen - get the length of string
+ * @str: the string
+ *
+ * Return: string length
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int i = 0;
 
+	if (str[i] == '\0')
+		return (0);
+	i++;
+	return (i + _strlen(str + i));
+}
 /**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: the binary number
@@ -8,18 +22,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int sum = 0;
+	unsigned int sum, i, length;
 
-	if (!b)
+	sum = 0, length = _strlen(b) - 1;
+
+	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		sum = 2 * sum + (b[i] - '0');
+		sum += (b[i] - '0') * (1 << length);
+		length -= 1;
 	}
-
 	return (sum);
 }
