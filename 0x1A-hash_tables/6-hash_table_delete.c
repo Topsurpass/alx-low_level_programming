e0dbf419abb3e636b15860855fbf43d387f54b0d
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-04-07 14:40:25 +0000

    last commit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -23,8 +23,9 @@ void hash_table_delete(hash_table_t *ht)
 	for (; i < ht->size; i++)
 	{
 		curr = ht->array[i];
-		while ((temp = curr) != NULL) /* free collision if an item is present */
+		while (curr != NULL) /* free collision if an item is present */
 		{
+			temp = curr;
 			curr = curr->next;
 			free(temp->key);
 			free(temp->value);
