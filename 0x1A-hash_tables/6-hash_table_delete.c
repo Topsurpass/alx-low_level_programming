3613635a576e2b59d2b2389c07f97e12665e2dde
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-04-07 19:28:16 +0000

    resolved merge issue

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,8 +9,8 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
 	hash_node_t *curr, *temp;
+	unsigned long int i;
 
 	if (ht == NULL)
 		return;
@@ -18,8 +18,9 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		curr = ht->array[i];
-		while ((temp = curr) != NULL)
+		while (curr != NULL)
 		{
+			temp = curr;
 			curr = curr->next;
 			free(temp->key);
 			free(temp->value);
