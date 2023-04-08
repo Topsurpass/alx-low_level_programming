fed8a25d672f72b24e2a5c032a0b62460c2ab777
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-04-08 16:57:46 +0000

    resolved bugs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -62,7 +62,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (item == NULL)
 		return (0);
 
-	item->next = NULL;
 	idx = key_index((const unsigned char *) key, ht->size);
 	temp = ht->array[idx];
 	if (temp == NULL) /* Check if the hash table array at this index is free */
