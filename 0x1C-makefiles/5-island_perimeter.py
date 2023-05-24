127ce9917f25ac99ca7b96a770ff4ff5da249896
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-05-24 22:19:37 +0000

    fix bugs

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-Module Island Perimeter
+This module defines Island Perimeter
 """
 
 
 def island_perimeter(grid):
     """ Calculate perimeter of grid where "1" is found"""
-    perimeter= 0
+    perimeter = 0
+
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
                 if row == 0 or grid[row - 1][col] == 0:
-                    perimeter+= 1
+                    perimeter += 1
                 if row == (len(grid) - 1) or grid[row + 1][col] == 0:
-                    perimeter+= 1
+                    perimeter += 1
                 if col == 0 or grid[row][col - 1] == 0:
-                    perimeter+= 1
+                    perimeter += 1
                 if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
-                    perimeter+= 1
+                    perimeter += 1
     return perimeter
